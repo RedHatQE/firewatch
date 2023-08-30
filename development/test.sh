51a4876f9c5d452fe097b21f6b53994fbf5eb235
Caleb Evans <caevans@redhat.com>
2023-08-30 15:46:04 -0600

    Remove test.sh

--- development/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build Jira config
-echo $JIRA_TOKEN > /tmp/token
-firewatch jira_config_gen --token_path /tmp/token --server_url $JIRA_SERVER_URL
-
-# Execute Firewatch
-firewatch report
