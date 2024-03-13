d484a19794ec3ce468e960e87f205362bcaaf6ba
Caleb Evans <caevans@redhat.com>
2024-03-13 00:26:59 -0600

    Modify test_renter_jira_config to use jira_token fixture

--- tests/unittests/utils/test_general.py
+++ tests/unittests/utils/test_general.py
@@ -8,13 +8,11 @@ def test_get_file_contents(tmp_path_factory, jira_token_path, jira_token):
     assert get_file_contents(file_path=jira_token_path) == jira_token
 
 
-def test_render_jira_config(tmp_path_factory):
+def test_render_jira_config(tmp_path_factory, jira_token):
     tmp_dir = tmp_path_factory.mktemp("jira_config")
     test_output_file = render_jira_config(
-        server_url=DEFAULT_JIRA_SERVER_URL, token="some-token", output_file=str(tmp_dir / "jira_config.json")
+        server_url=DEFAULT_JIRA_SERVER_URL, token=jira_token, output_file=str(tmp_dir / "jira_config.json")
     )
-
     with open(test_output_file, "r") as file:
         output = json.load(file)
-        assert output["url"] == DEFAULT_JIRA_SERVER_URL
-        assert output["token"] == "some-token"
+        assert output["url"] == DEFAULT_JIRA_SERVER_URL and output["token"] == jira_token
